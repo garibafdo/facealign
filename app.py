b83b4537546f46e18ffa7d67e5771ee6259ee23a
Your Name <you@example.com>
2025-09-23 12:48:37 +0200

    herpderp

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     scale = args.scale
 
     detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmark.dat")
 
     img = cv2.imread(input_image, cv2.IMREAD_GRAYSCALE)
     height, width = img.shape[:2]
